12645c4bdeb53aaf77b29deeeffd81f8c6729395
Foxicution <62815446+Foxicution@users.noreply.github.com>
2022-08-22 00:14:12 +0300

    Composing side effects on the boundary in main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from requests import get, put
 from datetime import datetime, timedelta
-from toolz.functoolz import pipe
+from toolz.functoolz import pipe, curry
 from functools import partial as p
 from structs import Info
+from typing import Iterable
+
+
+@curry
+def unpack(func, args, kwargs=None):
+    if kwargs is None:
+        kwargs = {}
+    return func(*args, **kwargs)
 
 
 def today() -> datetime:
@@ -42,17 +50,17 @@ def filter_lists(lst: list[dict]) -> list[Info]:
     return [Info(i[1], i[0], item) for i, item in sorted(dictionary.items(), key=lambda i: i[0][1])]
 
 
-def get_id_daytime(secret_key, secret_token):
-    return zip(pipe(get_lists(secret_key, secret_token), filter_lists),
-               pipe(today(), construct_week))
+def compose_info_week(lists: dict, curr_day: datetime) -> Iterable[tuple[Info, datetime]]:
+    return zip(pipe(lists, filter_lists),
+               pipe(curr_day, construct_week))
 
 
-def add_positions(lst: list[tuple[Info, datetime]]) -> list[tuple[Info, datetime, float]]:
+def add_positions(lst: list[tuple[Info, datetime]]) -> Iterable[tuple[Info, datetime, float]]:
     positions = [162046.82031345367, 210302.4804701805, 234430.31054854393, 258558.14062690735, 258558.57031345367]
     return pipe(lst, p(sorted, key=lambda x: x[1].day), p(zip, positions), p(map, lambda x: (x[1][0], x[1][1], x[0])))
 
 
-def set_list(secret_key, secret_token, item: tuple[Info, datetime, float]):
+def set_list(secret_key: str, secret_token: str, item: tuple[Info, datetime, float]) -> str:
     url = 'https://api.trello.com/1/lists/{}/name'.format(item[0].id)
     query = {
         'key': secret_key,
@@ -65,8 +73,12 @@ def set_list(secret_key, secret_token, item: tuple[Info, datetime, float]):
 
 def main():
     from config import key, token
-
-    pipe(get_id_daytime(key, token), add_positions, p(map, p(set_list, key, token)), list, print)
+    pipe((get_lists(key, token), today()),
+         unpack(compose_info_week),
+         add_positions,
+         p(map, p(set_list, key, token)),
+         list,
+         print)
 
 
 if __name__ == "__main__":
